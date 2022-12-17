2faceeac89c535b7688df6c30f99aad2b55ca434
Pilar <119014521+Pilar1992@users.noreply.github.com>
2022-12-17 04:04:03 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+function devinfo() {
+  alert ("Información de la desarrolladora. Nombre: Pilar Barraguirre. De Santa Fe, Argentina.")
+}
+
+function modonocturno() {
+  document.body.style.backgroundColor = "black";
+}
